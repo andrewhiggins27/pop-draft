04e536bc14f61a64c77cc9b01b6f71e6ebd9f880
Andrew Higgins <andrewhiggins27@gmail.com>
2020-08-12 16:39:55 -0400

    adjusted waiting games styling

--- app/javascript/react/components/WaitingGame.js
+++ app/javascript/react/components/WaitingGame.js
@@ -4,11 +4,11 @@ import { Link } from 'react-router-dom'
 const WaitingGame = props => {
 
   return (
-    <div className="callout cell large-3 small-6">
+    <div className="cell waiting-room-games large-3 small-6">
       <Link to={`/games/${props.id}/online`}>
         <div>
-          <h4>Created by: {props.creator}</h4>
-          <h4>Created at: {props.createdAt}</h4>
+          <h4 className="waiting-games-text">Created by: {props.creator}</h4>
+          <h4 className="waiting-games-text">Created at: {props.createdAt}</h4>
           <h5>{props.numberOfPlayers} player Draft</h5>
         </div>
       </Link>
