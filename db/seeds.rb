f1409d869a9b395875aee7250667f1e059bc4bb9
Andrew Higgins <andrewhiggins27@gmail.com>
2020-08-12 21:59:42 -0400

    added 3 new draft pools to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,23 @@
 require 'faraday'
 require 'faraday_middleware'
 
-Selection.destroy_all
-Pool.destroy_all
-Game.destroy_all
+# Selection.destroy_all
+# Pool.destroy_all
+# Game.destroy_all
 
 ## - First 150 pokemon - ##
-PokemonSeeder.seed!
+# PokemonSeeder.seed!
+
 ## - Miyazaki Characters - ##
-MiyazakiSeeder.seed!
+# MiyazakiSeeder.seed!
+
 ## - Comic Book Superheros - ##
-SuperheroSeeder.seed!
+# SuperheroSeeder.seed!
+
+## - Pixar Movies - ##
+TMDBSeeder.pixar_seed!
+
+## - Denzel Movies - ##
+TMDBSeeder.denzel_seed!
+
+MortySeeder.seed!
